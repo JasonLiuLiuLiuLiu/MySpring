71f380e9265d08794900bd3447987ced2b8f355e
Zhenyu,Liu <liuzhenyulive@live.com>
2019-10-07 15:46:13 +0800

    Add Jpa demo

---
+++ 01Jpa/src/main/java/My/Spring/Coffee/Jpa/Model/OrderState.java
@@ -0,0 +1,5 @@
+package My.Spring.Coffee.Jpa.Model;
+
+public enum  OrderState {
+    INIT, PAID, BREWING, BREWED, TAKEN, CANCELLED
+}
---
+++ 01Jpa/src/main/java/My/Spring/Coffee/Jpa/Repository/CoffeeRepository.java
@@ -0,0 +1,6 @@
+package My.Spring.Coffee.Jpa.Repository;
+
+import My.Spring.Coffee.Jpa.Model.Coffee;
+
+public interface CoffeeRepository extends BaseRepository<Coffee,Long> {
+}
