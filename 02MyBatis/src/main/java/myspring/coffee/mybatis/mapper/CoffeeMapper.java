f1efb04a79ba5a56bdd72951144c944de66f3f6c
Zhenyu,Liu <liuzhenyulive@live.com>
2019-10-09 22:10:08 +0800

    add  mybatis

---
+++ 02MyBatis/src/main/java/myspring/coffee/mybatis/mapper/CoffeeMapper.java
@@ -0,0 +1,18 @@
+package myspring.coffee.mybatis.mapper;
+
+import myspring.coffee.mybatis.model.Coffee;
+import org.apache.ibatis.annotations.*;
+
+@Mapper
+public interface CoffeeMapper {
+    @Insert("insert into t_coffee (name,price,create_time,update_time)"+"values(#{name},#{price},now(),now())")
+    @Options(useGeneratedKeys = true,keyProperty = "id")
+    int save(Coffee coffee);
+
+    @Select("select * from t_coffee where id=#{id}")
+    @Results({
+            @Result(id = true,column = "id",property = "id"),
+            @Result(column = "create_time",property = "createTime"),
+    })
+    Coffee findById(@Param("id") long id);
+}
