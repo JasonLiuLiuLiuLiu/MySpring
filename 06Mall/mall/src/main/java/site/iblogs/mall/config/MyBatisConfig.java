5fae00f1b3615baa8b7d2f44d1415e5cfd331787
Zhenyu,Liu <liuzhenyulive@live.com>
2020-01-15 00:50:22 +0800

    update mybatis

--- 06Mall/mall/src/main/java/site/iblogs/mall/config/MyBatisConfig.java
+++ 06Mall/mall/src/main/java/site/iblogs/mall/config/MyBatisConfig.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("site.iblogs.mall.mbg.mapper")
+@MapperScan("site.iblogs.site.mbg.mapper")
 public class MyBatisConfig {
+
 }
