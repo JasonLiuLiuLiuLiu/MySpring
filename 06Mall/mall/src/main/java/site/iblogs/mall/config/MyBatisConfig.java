eff6b926293f182630834d2bee529978978b6b93
Zhenyu,Liu <liuzhenyulive@live.com>
2020-01-18 01:20:54 +0800

    fix jwt issue

--- 06Mall/mall/src/main/java/site/iblogs/mall/config/MyBatisConfig.java
+++ 06Mall/mall/src/main/java/site/iblogs/mall/config/MyBatisConfig.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("site.iblogs.mall.mbg.mapper")
+@MapperScan({"site.iblogs.mall.mbg.mapper","site.iblogs.mall.dao"})
 public class MyBatisConfig {
 
 }
