c3d7118cbe4e1c51cf1959e56e94cf96e36a2e74
Zhenyu,Liu <liuzhenyulive@live.com>
2020-01-21 01:00:40 +0800

    add spring task

---
+++ 06Mall/mall/src/main/java/site/iblogs/mall/config/SpringTaskConfig.java
@@ -0,0 +1,9 @@
+package site.iblogs.mall.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
