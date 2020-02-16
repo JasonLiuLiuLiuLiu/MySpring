01baead151743f33b399eae1001694fb474da511
Zhenyu,Liu <liuzhenyulive@live.com>
2020-02-16 22:39:40 +0800

    虚假唤醒

--- 07JUC/JUC/src/main/java/site/iblogs/lock/ProducerAndConsumer.java
+++ 07JUC/JUC/src/main/java/site/iblogs/lock/ProducerAndConsumer.java
@@ -38,8 +38,12 @@ class Test{
     private boolean produced = false;
 
     public synchronized void Producer() throws Exception {
-        if(produced)
-        {
+// 可能会产生虚假唤醒
+//        if(produced)
+//        {
+//            this.wait();
+//        }
+        while (produced){
             this.wait();
         }
         System.out.println("In Producer:" + Thread.currentThread().getName());
@@ -48,14 +52,26 @@ class Test{
     }
 
     public synchronized void Consumer() throws Exception {
-        if(!produced)
-        {
+// 可能会产生虚假唤醒
+//        if(!produced)
+//        {
+//            this.wait();
+//        }
+        if(!produced){
             this.wait();
         }
         System.out.println("In Consumer:" + Thread.currentThread().getName());
         produced=false;
         this.notify();
     }
+
+/*    怎么产生虚假唤醒
+    把  while (product >=1) {}
+    换成   if (product >=1) {}
+    就会出现虚假唤醒
+    为什么 if会出现虚假唤醒
+    因为if只会执行一次，执行完会接着向下执行if（）外边的
+    而while不会，直到条件满足才会向下执行while（）外边的*/
 
 }
 
