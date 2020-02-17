e2f77253dd5c47db6e3b2c2122e935f3c23d6efd
Zhenyu,Liu <liuzhenyulive@live.com>
2020-02-17 23:47:01 +0800

    Create StreamDemo.java

---
+++ 07JUC/JUC/src/main/java/site/iblogs/steram/StreamDemo.java
@@ -0,0 +1,64 @@
+package site.iblogs.steram;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 一下数据，进行操作筛选用户：要求：一行代码做出此题，时长1分钟！
+ * 1、全部满足偶数ID
+ * 2、年龄都大于24
+ * 3、用户名转为大写
+ * 4、用户名字母倒排序
+ * 5、只能输出一个名字
+ */
+public class StreamDemo {
+    public static void main(String[] args) {
+        User u1 = new User(11, "a", 23);
+        User u2 = new User(12, "b", 24);
+        User u3 = new User(13, "c", 22);
+        User u4 = new User(14, "d", 28);
+        User u5 = new User(16, "e", 26);
+
+        // 集合管理数据
+        List<User> list = Arrays.asList(u1, u2, u3, u4, u5);
+        // 计算交给Stream
+        // 过滤  filter
+        // 映射 map
+        // 排序， sort
+        // 分页   limit
+        list.stream()
+                .filter(u->{return u.getId()%2==0;})
+                .filter(u->{return u.getAge()>24;})
+                .map(u->{return u.getUsername().toUpperCase();})
+                .sorted((o1,o2)->{return o2.compareTo(o1);})
+                .limit(1)
+                .forEach(System.out::println);
+
+        // 泛型、注解、反射
+        // 链式编程 + 流式计算 + lambda表达式
+
+    }
+}
+
+class User{
+    public int getId() {
+        return id;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getUsername() {
+        return userName;
+    }
+
+    private int id;
+    private int age;
+    private String userName;
+    public User(int id,String name,int age){
+        this.id=id;
+        this.age=age;
+        this.userName=name;
+    }
+}
